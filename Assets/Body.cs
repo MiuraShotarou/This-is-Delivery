57b9971cf3f31e3b83b6669542de2ac154145c3c
miurasyoutaro <miurasyoutaro@gmail.com>
2025-12-12 09:57:29 +0900

    <Body.cs> ・CarState, 及び、Update関数内で関数を実行させる処理を記述した

--- Assets/Body.cs
+++ Assets/Body.cs
@@ -8,7 +8,6 @@ public class Body : MonoBehaviour
     Timer _timer;
     Engine _engine;
     CarState _carState;
-    // public Engine Engine {set => _engine = value; }
     public CarState CarState {set => _carState = value; }
     void Awake()
     {
@@ -30,9 +29,14 @@ public class Body : MonoBehaviour
             case CarState.DriftHigh:
                 break;
             case CarState.Boost:
+                Boost();
+                break;
+            case CarState.BoostStay:
+                BoostStay();
                 break;
             case CarState.BoostAttenuation:
                 BoostAttenuation();
+                SpeedCheck();
                 break;
         }
     }
@@ -41,7 +45,7 @@ public class Body : MonoBehaviour
         // 車同士が衝突するとお互いに反発し合う
         if (collision.gameObject.CompareTag("Cart"))
         {
-            if (_timer._CanAddForce)
+            if (_timer.IsCanAddForce)
             {
                 AddForce(collision.gameObject.GetComponent<Body>());
                 _timer.InitAddForceParam();
@@ -54,5 +58,36 @@ public class Body : MonoBehaviour
         float forcePower = body.Weight - Weight + GameDataManager.Instance.CarStateMultiplier[(int)_carState];
         _rigidbody.AddForce(reverse * forcePower, ForceMode.Impulse);
     }
-    void BoostAttenuation() => _engine.CurrentSpeed -= 
+
+    void Boost()
+    {
+        // ブースト処理
+        float boostAdditve = GameDataManager.Instance.BoostAddtive;
+        _engine.CurrentSpeed += boostAdditve;
+        _engine.TheoreticallySpeed += boostAdditve;
+        // タイマーのスタート
+        _timer.InitAddForceParam();
+        // BoostStay中はTimer.Updateを監視する
+        CarState = CarState.BoostStay;
+        //基礎スピード + ピザ + ペッパー
+    }
+
+    void BoostStay()
+    {
+        if (_timer.IsCanAttenuation)
+        {
+            CarState = CarState.BoostAttenuation;
+        }
+    }
+    /// <summary> 減速処理 </summary>
+    void BoostAttenuation() => _engine.CurrentSpeed -= GameDataManager.Instance.AttenuationMultiplier;
+    /// <summary> 現在の速度があるべき速度よりも下回っていた際に、正しいスピードへ戻すための関数 </summary>
+    void SpeedCheck()
+    {
+        if (_engine.CurrentSpeed < _engine.TheoreticallySpeed)
+        {
+            _engine.CurrentSpeed = _engine.TheoreticallySpeed;
+            CarState = CarState.Normal; //誤作動の危険
+        }
+    }
 }
